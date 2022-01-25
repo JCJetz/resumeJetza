2ed42edf5bc926b2a24abd4a2eb56eb645a9f0be
JCJetz <jetzabel@gmail.com>
2022-01-25 20:50:43 +0100

    añadido gh-pages a build-assets

--- scripts/build-assets.js
+++ scripts/build-assets.js
@@ -1,5 +1,11 @@
 'use strict';
 
 const renderAssets = require('./render-assets');
+var ghpages = require('gh-pages');
 
 renderAssets();
+
+console.log('ejecutando gh-pages');
+ghpages.publish('dist', function(err) {});
+console.log('gh-pages ejecutado');
+
