359b120aa27645d2431a3f2d4813752c6e6b8c1a
JCJetz <jetzabel@gmail.com>
2022-01-25 20:51:18 +0100

    Updates

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+/*!
+* Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// hoover foto de perfil
+
+const  pfpimage = document.querySelector(".img-profile")
+pfpimage.addEventListener('mouseover', function (e) {
+    e.target.src = 'assets/img/profile.jpg'
+})
+pfpimage.addEventListener('mouseout',  function (e) {
+    e.target.src = 'https://pbs.twimg.com/profile_images/1421046483676241921/cMbGnPBm_400x400.jpg'
+});
